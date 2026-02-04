3d608aad102cfeedc7879f29feccb7d7e47a6b29
anayethasan <mdneloy256@gmail.com>
2026-02-05 00:13:19 +0600

    user login log out system done

--- user_management/settings.py
+++ user_management/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'user_management',
+    'user',
+    'core',
     "debug_toolbar",
 ]
 
--- user_management/urls.py
+++ user_management/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
-from django.urls import path
+from django.urls import path, include
 from tasks.views import manager_dashboard, user_dashboard, toppart, test, create_task, view_task, update_task, delete_task
+from core.views import home
 
 urlpatterns = [
-    path("/", manager_dashboard, name='manager_dashboard'),
     path("dashboard/", toppart),
     path("manager_dashboard/", manager_dashboard, name='manager_dashboard'),
     path("user_dashboard/", user_dashboard),
@@ -28,5 +28,7 @@ urlpatterns = [
     path("create-task/", create_task, name='create-task'),
     path("view_task/", view_task),
     path("update_task/<int:id>/", update_task, name='update_task'),
-    path("delete_task/<int:id>/", delete_task, name='delete_task')
+    path("delete_task/<int:id>/", delete_task, name='delete_task'),
+    path('user/', include("user.urls")),
+    path('', home),
 ] + debug_toolbar_urls()
