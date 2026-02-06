f433ae3f6aeaf21549b3aea4ac8e0ee40250a82d
anayethasan <mdneloy256@gmail.com>
2026-02-06 12:44:31 +0600

    envirourment set

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tasks.models import Employee, Project, Task, TaskDetail
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(TaskDetail)
--- user_management/urls.py
+++ user_management/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for user_management project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.urls import path, include
@@ -21,6 +5,7 @@ from tasks.views import manager_dashboard, user_dashboard, toppart, test, create
 from core.views import home
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("dashboard/", toppart),
     path("manager_dashboard/", manager_dashboard, name='manager_dashboard'),
     path("user_dashboard/", user_dashboard),
